4f56131385d6ab2da7318fec754dac6047c0d9ad
Equinox-13 <quaid.johar33@gmail.com>
2020-08-16 19:38:12 +0530

    Fetched data through django_rest api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,31 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import axios from 'axios';
 
-const list = [
-  {
-    "id":1,
-    "title":"1st todo",
-    "body":"Learn Django properly."
-  },
-  {
-    "id":2,
-    "title":"Second item",
-    "body":"Learn Python."
-  },
-  {
-    "id":3,
-    "title":"Learn HTTP",
-    "body":"It's important."
-  }
-]
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { list };
+  state = {
+    todos: []
+  }
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then(res => {
+        this.setState({ todos: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div>
-        {this.state.list.map(item => (
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
             <p>{item.body}</p>
@@ -40,26 +36,5 @@ class App extends Component {
   }
 
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
